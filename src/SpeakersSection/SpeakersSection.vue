80e974c075e7bfaec37727bbc9502816dfd95e68
Gevorg Harutyunyan <gevharut@gmail.com>
2022-08-13 00:52:17 +0800

    Implement speaker boxes
    Signed-off-by: Gevorg Harutyunyan <gevharut@gmail.com>

--- src/SpeakersSection/SpeakersSection.vue
+++ src/SpeakersSection/SpeakersSection.vue
@@ -1,13 +1,29 @@
 <template>
-  <div id="Speakers" class="min-h-screen bg-[#f9ad1b] py-8">
+  <div id="Speakers" class="min-h-screen bg-[#f9ad1b] pt-8 pb-20">
     <div class="container mx-auto">
-      <div class="mb-8 text-center">
-        <h3 class="text-4xl text-[#912396]">Speakers</h3>
+      <div class="mb-16 text-center">
+        <h3 class="font-heading text-3xl text-[#912396]">Speakers</h3>
+      </div>
+
+      <div class="grid grid-cols-3 gap-x-4 gap-y-20">
+        <div
+          v-for="speaker in speakers"
+          :key="speaker.id"
+          class="flex flex-row justify-center"
+        >
+          <SpeakerItem />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import SpeakerItem from '../_components/SpeakerItem/SpeakerItem.vue'
+
+  const speakers = new Array(12).fill(null).map((item, index) => ({
+    id: index,
+  }))
+</script>
 
 <style lang="scss" scoped></style>
--- src/_components/SpeakerItem/SpeakerItem.vue
+++ src/_components/SpeakerItem/SpeakerItem.vue
@@ -1,7 +1,27 @@
 <template>
-  <div></div>
+  <div class="relative">
+    <div class="SpeakerCardBg h-60 w-56"></div>
+    <div class="SpeakerCard h-60 w-56"></div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .SpeakerCard {
+    background-color: #fff;
+    border: 7px solid #252e3a;
+    position: relative;
+    z-index: 1;
+  }
+
+  .SpeakerCardBg {
+    border: 7px solid #252e3a;
+    background: #fff;
+    position: absolute;
+    left: 20px;
+    top: 20px;
+    background-image: url('./stripes.svg');
+    z-index: 0;
+  }
+</style>
