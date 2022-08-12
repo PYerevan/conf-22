fdde8d85df43261364cbd422ccb30565030fb783
Gevorg Harutyunyan <gevharut@gmail.com>
2022-08-12 23:33:56 +0800

    setup eslint and tailwind

---
+++ src/IntroSection/IntroSection.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="container mx-auto flex min-h-screen flex-col items-center justify-center"
+  >
+    <h1 class="font-heading mb-4 text-5xl">PyCon Armenia</h1>
+    <p>November 5-6, AUA</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
